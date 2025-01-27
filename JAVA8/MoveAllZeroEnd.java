d8834c532e1026e5ced5790b228a65e93ce449d2
Akash Kumar <73156645+akash9721@users.noreply.github.com>
2025-01-27 23:59:22 +0530

    Update MoveAllZeroEnd.java using simple app

--- JAVA8/MoveAllZeroEnd.java
+++ JAVA8/MoveAllZeroEnd.java
@@ -20,3 +20,51 @@ public class MoveAllZeroEnd {
 	}
 
 }
+
+
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++){
+           if(nums[i]!=0){
+            arr[j++]=nums[i];
+           }
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=arr[i];
+        }
+    }
+}
+
+
+#using 2 pointer
+
+class Solution {
+     public void moveZeroes(int[] nums) {
+       // move all non zero to left
+       int left=0;
+       for(int right=0;right<nums.length;right++){
+        if(nums[right]!=0){
+            int temp=nums[right];
+            nums[right]=nums[left];
+            nums[left]=temp;
+            left++;
+         }
+       }
+    }
+}
+
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
