adc1c349efee40804a6dd54d681a80725608cc3b
Akash Kumar <73156645+akash9721@users.noreply.github.com>
2025-02-09 00:48:44 +0530

    Create SecondHighestSalary.java in non repeted data

---
+++ JAVA8/SecondHighestSalary.java
@@ -0,0 +1,27 @@
+package com.java8;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+public class SecondHighestSalary {
+
+	public static void main(String[] args) {
+		Map<String, Integer> map=new HashMap<>();
+		map.put("anil", 1000);
+		map.put("bhavna", 1300);
+		map.put("michel", 1500);
+		map.put("tom", 1600);
+		map.put("ankit", 1200);
+		map.put("danial", 1700);
+		map.put("james", 1400);
+		
+		Entry<String, Integer> secondHighestElement = map.entrySet().stream()
+			.sorted(Map.Entry.<String,Integer>comparingByValue().reversed())
+		    .collect(Collectors.toList())
+		    .get(1);
+		System.out.println(secondHighestElement);
+	}
+}
