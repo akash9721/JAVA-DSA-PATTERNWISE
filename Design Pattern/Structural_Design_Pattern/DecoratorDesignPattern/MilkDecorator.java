d6197dd35101085cf426105a8cb480cca62eb325
Akash Kumar <73156645+akash9721@users.noreply.github.com>
2025-02-20 00:33:00 +0530

    Create MilkDecorator.java

---
+++ Design Pattern/Structural_Design_Pattern/DecoratorDesignPattern/MilkDecorator.java
@@ -0,0 +1,17 @@
+// Concrete Decorator
+class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 20; // Adding Milk cost
+    }
+}
+//Enhances coffee description & cost dynamically.
