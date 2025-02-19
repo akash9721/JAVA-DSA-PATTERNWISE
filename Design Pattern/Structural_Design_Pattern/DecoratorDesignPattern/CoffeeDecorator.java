758858506162e506b64a11cca38a41fed287e7af
Akash Kumar <73156645+akash9721@users.noreply.github.com>
2025-02-20 00:31:19 +0530

    Create CoffeeDecorator.java

---
+++ Design Pattern/Structural_Design_Pattern/DecoratorDesignPattern/CoffeeDecorator.java
@@ -0,0 +1,19 @@
+// Abstract Decorator
+abstract class CoffeeDecorator implements Coffee {
+    protected Coffee coffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost();
+    }
+}
+//The decorator class wraps the coffee object and allows adding new behavior.
