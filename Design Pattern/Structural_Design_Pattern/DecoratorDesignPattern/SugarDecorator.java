37a7b8981e7a0d4463877fde00c179ad02ff43ef
Akash Kumar <73156645+akash9721@users.noreply.github.com>
2025-02-20 00:33:47 +0530

    Create SugarDecorator.java

---
+++ Design Pattern/Structural_Design_Pattern/DecoratorDesignPattern/SugarDecorator.java
@@ -0,0 +1,17 @@
+// Concrete Decorator
+class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 10; // Adding Sugar cost
+    }
+}
+//Sugar costs extra and is added dynamically.
