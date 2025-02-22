6488f6967a8889d8d71c92d0bfe3297119dc115a
Akash Kumar <73156645+akash9721@users.noreply.github.com>
2025-02-23 01:10:49 +0530

    Create CustomUncheckedExceptionDemo.java

---
+++ EXCEPTION-HANDLING/CustomUncheckedExceptionDemo.java
@@ -0,0 +1,11 @@
+package com.exception;
+
+public class CustomUncheckedExceptionDemo {
+
+	public static void main(String[] args) {
+		VotingSystem vs=new VotingSystem();
+		vs.checkEligibility(17);
+		
+	}
+	
+}
